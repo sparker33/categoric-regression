1508095019c5e1c13b1e9f3925316548a2b9011c
sparker33 <sparker1491@gmail.com>
2022-05-14 08:34:34 -0500

    Improved plots and refactor

--- arrange_sim.py
+++ arrange_sim.py
@@ -1,20 +1,23 @@
 import pandas as pd
+import numpy as np
 from agent_point import agent_point
 
 class arrange_sim:
-    def __init__(self, data: pd.DataFrame, category_dims_count:int, values_dim,
-                values_force_const:float, ambient_force_const:float,
-                values_order_const:float = 1.0, ambient_order_const:float = 0.0, 
+    def __init__(self, data: pd.DataFrame, category_dims:list, values_dim,
+                values_force_const:float, categoriees_force_const:float, ambient_force_const:float,
+                values_order_const:float = 1.0, categories_order_const:float = 0.0, ambient_order_const:float = 0.0, 
                 damping_const:float = 0.05, dist_thresh:float = 10**(-10)):
-        self.c_vals = values_force_const # positive values repel, proportional to difference in values
+        self.c_vals = values_force_const # positive values repel dissimilar, proportional to difference in values
+        self.c_categories = categoriees_force_const # positive values repel dissimilar categories, const
         self.c_ambient = ambient_force_const # positive values repel, const
         self.o_vals = values_order_const # sets order of distance exponential in force calc denominator for values
+        self.o_categories = categories_order_const # sets order of distance exponential in force calc denominator for categories
         self.o_ambient = ambient_order_const # sets order of distance exponential in force calc denominator for ambient
         self.damping = damping_const # damping on particle velocities
         self.dist_thresh = dist_thresh # cutoff proximity for force calculations (prevents ~infinite forces)
         self.agents = []
         for row in data.iterrows():
-            self.agents.append(agent_point(row[0], row[1][values_dim], category_dims_count))
+            self.agents.append(agent_point(row[0], row[1][values_dim], row[1][category_dims]))
         return
 
     def get_positions_df(self) -> pd.DataFrame:
@@ -43,11 +46,26 @@ class arrange_sim:
                 a.clear_forces()
             for i,a1 in enumerate(self.agents[:-1]):
                 for a2 in self.agents[i+1:]:
-                    f = a1.calculate_forces(a2, c_vals=self.c_vals, c_ambient=self.c_ambient,
-                                            o_vals=self.o_vals, o_ambient=self.o_ambient, dist_thresh=self.dist_thresh)
+                    f = self.calculate_forces(a1, a2)
                     a1.update_forces(f)
                     a2.update_forces(-f)
                 a1.update_kinematics(increment, self.damping)
             self.agents[-1].update_kinematics(increment, self.damping)
             t += increment
         return
+
+    def calculate_forces(self, agent1:agent_point, agent2:agent_point) -> np.array:
+        assert len(agent1.pos) == len(agent2.pos)
+        direction = agent1.pos - agent2.pos
+        distance = np.linalg.norm(direction)
+        force_magnitude = 0.0
+        if distance > self.dist_thresh:
+            force_magnitude += ((self.c_vals * abs(agent1.val - agent2.val) / (distance**self.o_vals)) + (self.c_ambient / (distance**self.o_ambient)))
+        category_force = (self.c_categories / (distance**self.o_categories))
+        for i,cat in enumerate(agent1.categories):
+            if agent2.categories[i] != cat:
+                force_magnitude += category_force
+            else:
+                force_magnitude -= category_force
+        force_increment = force_magnitude * direction
+        return force_increment
--- main_script.py
+++ main_script.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from scipy import rand
 from regressor import regressor
 from arrange_sim import arrange_sim
@@ -11,35 +12,68 @@ base_regression = regressor(data, cardinal_dims_indexes, result_dim_index)
 base_regression.print_regression_string()
 # base_regression.plot_regression()
 
-
 data['Base Regression'] = base_regression.get_Y_pred_Series()
-data['Base Error'] = data['Base Regression'] - data['Result']
+result_norm = sum([abs(r) for r in data['Result']]) / len(data['Result'])
+data['Base Error'] = (data['Base Regression'] - data['Result']) / result_norm
 
-sim = arrange_sim(data, 2, 'Base Error', values_force_const=0.008, ambient_force_const=-0.05,
-                    values_order_const=1.0, ambient_order_const=0.0, damping_const=0.1, dist_thresh=10**(-5))
-# sim_energy = []
-# for i in range(150):
-#     sim.run_sim()
-#     energy = sim.get_energy()
-#     sim_energy.append([i, energy[0], energy[1]])
-# sim_energy = pd.DataFrame(sim_energy, columns=['Iteration', 'Potential', 'Kinetic'])
+category_dims = ['Category 1', 'Category 2']
+vals_dim = 'Base Error'
+sim = arrange_sim(data, category_dims, vals_dim, values_force_const=0.012, categoriees_force_const=0.1, ambient_force_const=-0.2,
+                    values_order_const=1.0, categories_order_const=0.0, ambient_order_const=0.0, damping_const=0.33, dist_thresh=10**(-8))
 
-# import matplotlib.pyplot as plt
-# plt.plot(sim_energy['Iteration'], sim_energy['Potential'])
-# plt.plot(sim_energy['Iteration'], sim_energy['Kinetic'])
-# plt.show()
-
+################
+#### PROTOTYPING PLOTS 1
+################
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-fig = plt.figure()
-ax = fig.add_axes([0,0,1,1], frameon=False)
-ax.set_xlim(0,1), ax.set_xticks([])
-ax.set_ylim(0,1), ax.set_yticks([])
-scat = ax.scatter(x=[a.pos[0] for a in sim.agents], y=[a.pos[1] for a in sim.agents], c=[a.val for a in sim.agents], cmap='Greens')
-def update(frame):
+fig,axs = plt.subplots(2,2)
+ID1_colors = {'A':1, 'B':2, 'C':3, 'D':4, 'Z':5}
+ID2_colors = {'ID1':1, 'ID2':2, 'ID3':3, 'ID4':4, 'ID5':5}
+scat_vals = axs[0,0].scatter(x=[a.pos[0] for a in sim.agents], y=[a.pos[1] for a in sim.agents], c=[a.val for a in sim.agents], cmap='YlGnBu')
+scat_cat1 = axs[0,1].scatter(x=[a.pos[0] for a in sim.agents], y=[a.pos[1] for a in sim.agents], c=[ID1_colors[id] for id in list(data['Category 1'])], cmap='Spectral')
+scat_cat2 = axs[1,0].scatter(x=[a.pos[0] for a in sim.agents], y=[a.pos[1] for a in sim.agents], c=[ID2_colors[id] for id in list(data['Category 2'])], cmap='Spectral')
+times = [0.0]
+sim_energy = [[0.0], [0.0]]
+lines = [axs[1,1].plot([], [])[0] for _ in sim_energy]
+def update(frame, lines):
     sim.run_sim(duration=0.1, increment=0.1)
-    scat.set_offsets([a.pos for a in sim.agents])
+    scat_vals.set_offsets([[a.pos[0],a.pos[1]] for a in sim.agents])
+    scat_cat1.set_offsets([[a.pos[0],a.pos[1]] for a in sim.agents])
+    scat_cat2.set_offsets([[a.pos[0],a.pos[1]] for a in sim.agents])
+    energy = sim.get_energy()
+    times.append(times[-1]+0.1)
+    axs[1,1].set_xlim(0,times[-1])
+    axs[1,1].set_ylim(0,np.max([np.max(sim_energy[0]), np.max(sim_energy[1])]))
+    for i,line in enumerate(lines):
+        sim_energy[i].append(energy[i])
+        line.set_data(np.array([list(te) for te in zip(times, sim_energy[i])]).T)
     return
-
-anim = animation.FuncAnimation(fig, update, interval=100)
+anim = animation.FuncAnimation(fig, update, fargs=(lines,), interval=50)
 plt.show()
+################
+#### END PROTOTYPING PLOTS 1
+################
+
+################
+#### PROTOTYPING ORIENTING
+################
+arranged_points = np.array(sim.get_positions_df())
+w,v = np.linalg.eig(np.cov(arranged_points.T))
+v_ordered = v[np.argsort(w)]
+arranged_points_norm = (v_ordered @ arranged_points.T).T
+################
+#### END PROTOTYPING ORIENTING
+################
+
+################
+#### PROTOTYPING PLOTS 2
+################
+fig, axs = plt.subplots(2,2)
+axs[0,0].scatter(x=arranged_points[:,0], y=arranged_points[:,1], c=[val for val in list(data['Base Error'])], cmap='YlGnBu')
+axs[0,1].scatter(x=arranged_points_norm[:,0], y=arranged_points_norm[:,1], c=[val for val in list(data['Base Error'])], cmap='YlGnBu')
+axs[1,0].scatter(x=arranged_points_norm[:,0], y=arranged_points_norm[:,1], c=[ID1_colors[id] for id in list(data['Category 1'])], cmap='Spectral')
+axs[1,1].scatter(x=arranged_points_norm[:,0], y=arranged_points_norm[:,1], c=[ID2_colors[id] for id in list(data['Category 2'])], cmap='Spectral')
+plt.show()
+################
+#### END PROTOTYPING PLOTS 2
+################
